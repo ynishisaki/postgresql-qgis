d7d6263dc487dd6dd0db1e0287852cf9475f60ab
yu nishisaki <ynishisaki@gmail.com>
2022-11-01 18:36:44 +0900

    初回実行用sqlを追加

---
+++ script/create_table.sql
@@ -0,0 +1,9 @@
+\c qgisdb
+
+CREATE TABLE streets (id serial not null primary key, name varchar(50));
+
+CREATE TABLE people (id serial not null primary key,
+                     name varchar(50),
+                     house_no int not null,
+                     street_id int not null,
+                     phone_no varchar null );
