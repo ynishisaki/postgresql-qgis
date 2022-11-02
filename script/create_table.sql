671634fef78b92cd5b986eb9e97c40bb1840b5c4
yu nishisaki <ynishisaki@gmail.com>
2022-11-02 20:05:48 +0900

    postgis追加

--- script/create_table.sql
+++ script/create_table.sql
@@ -3,7 +3,14 @@
 CREATE TABLE streets (id serial not null primary key, name varchar(50));
 
 CREATE TABLE people (id serial not null primary key,
-                     name varchar(50),
-                     house_no int not null,
-                     street_id int not null,
-                     phone_no varchar null );
+  name varchar(50),
+  house_no int not null,
+  street_id int not null,
+  phone_no varchar null );
+
+-- CREATE TABLE address (
+--   id serial not null primary key,
+--   name varchar(50),
+--   house_no int not null,
+--   street_id int not null,
+--   phone_no varchar null );
